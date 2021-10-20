bf7960ea5cb4c82e4b4608b16d7c05039f20395a
wusamin <tatsusamimi@gmail.com>
2021-10-20 19:29:32 +0900

    設問1を改修
    残処理を実装
    無名型を定義した

--- question1.go
+++ question1.go
@@ -22,18 +22,15 @@ func Question1(filepath string) {
 
 	var failureIpAddrs map[string]int64 = map[string]int64{}
 
-	var failureList []struct {
-		IpAddress        string
-		StartFailureTime string
-		EndFailureTime   string
-	}
+	var failureList []*FailureServerDatum
 
 	for fileScanner.Scan() {
-		splitted := strings.Split(fileScanner.Text(), ",")
+		logLine := fileScanner.Text()
+		splitted := strings.Split(logLine, ",")
 
 		if len(splitted) != 3 {
 			// ログの形式が違う場合のワーニング
-			log.Println("")
+			log.Printf("ログの形式が異常です。ログ: %s", logLine)
 			continue
 		}
 
@@ -41,18 +38,18 @@ func Question1(filepath string) {
 
 		// 故障マップ内にIPアドレスがあるか確認
 		if val, ok := failureIpAddrs[ipAddr]; ok {
-			// ある場合は
-			failureList[val].EndFailureTime = splitted[0]
-			delete(failureIpAddrs, ipAddr)
+			// ある場合
+			if splitted[2] != "-" {
+				failureList[val].EndFailureTime = splitted[0]
+				delete(failureIpAddrs, ipAddr)
+			}
+			// 故障しているIPは復帰するまで処理不要
+			continue
 		}
 
-		// 故障（pingが通らない）の場合は故障マップにIPアドレスを追加する
 		if splitted[2] == "-" {
-			failureList = append(failureList, struct {
-				IpAddress        string
-				StartFailureTime string
-				EndFailureTime   string
-			}{
+			// 故障（pingが通らない）の場合は故障マップにIPアドレスを追加する
+			failureList = append(failureList, &FailureServerDatum{
 				ipAddr,
 				splitted[0],
 				"",
@@ -60,21 +57,26 @@ func Question1(filepath string) {
 			failureIpAddrs[ipAddr] = int64(len(failureList) - 1)
 		}
 	}
-	// handle first encountered error while reading
+
+	// ファイル読込中にエラーが起きた場合のハンドリング
 	if err := fileScanner.Err(); err != nil {
 		log.Fatalf("Error while reading file: %s", err)
 	}
 
+	exportFailureList(failureList)
+}
+
+func exportFailureList(failureList []*FailureServerDatum) {
 	for _, v := range failureList {
 		var endTime string
 		if v.EndFailureTime == "" {
 			endTime = "故障中"
 		} else {
-			// Pingが返ってきた寸前までが故障期間
+			// pingが返ってきた寸前までが故障期間
 			formatted, err := time.Parse("20060102150405", v.EndFailureTime)
+
 			if err != nil {
-				// TODO 日付フォーマット時のエラーのハンドリング
-				log.Println(err)
+				log.Printf("ログの日付形式が異常です。ログ: %s", v.EndFailureTime)
 			}
 
 			endTime = formatted.Add(-time.Second).Format("20060102150405")
@@ -82,4 +84,10 @@ func Question1(filepath string) {
 
 		log.Printf("IPアドレス: %s, 故障期間: %s - %s", v.IpAddress, v.StartFailureTime, endTime)
 	}
+}
+
+type FailureServerDatum struct {
+	IpAddress        string
+	StartFailureTime string
+	EndFailureTime   string
 }
