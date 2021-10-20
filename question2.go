d866e5701af697daec87fb04a08e211a17fd5e88
wusamin <tatsusamimi@gmail.com>
2021-10-20 20:08:55 +0900

    設問2の処理を実装

--- question2.go
+++ question2.go
@@ -9,7 +9,7 @@ import (
 )
 
 // Question2 is: 設問2の処理
-func Question2(filepath string) {
+func Question2(filepath string, tryCount int64) {
 	file, err := os.Open(filepath)
 
 	if err != nil {
@@ -20,61 +20,116 @@ func Question2(filepath string) {
 
 	fileScanner := bufio.NewScanner(file)
 
-	var failureIpAddrs map[string]int64 = map[string]int64{}
+	var failureIpAddrs map[string]struct {
+		FailureListIndex int64
+		TryCount         int64
+		StartTime        string
+	} = map[string]struct {
+		FailureListIndex int64
+		TryCount         int64
+		StartTime        string
+	}{}
 
-	var failureList []struct {
-		IpAddress        string
-		StartFailureTime string
-		EndFailureTime   string
-	}
+	var failureList []*FailureServerDatum
 
 	for fileScanner.Scan() {
-		splitted := strings.Split(fileScanner.Text(), ",")
+		logLine := fileScanner.Text()
+		splitted := strings.Split(logLine, ",")
 
 		if len(splitted) != 3 {
 			// ログの形式が違う場合のワーニング
-			log.Println("")
+			log.Printf("ログの形式が異常です。ログ: %s", logLine)
 			continue
 		}
 
 		ipAddr := splitted[1]
-
 		// 故障マップ内にIPアドレスがあるか確認
 		if val, ok := failureIpAddrs[ipAddr]; ok {
-			// ある場合は
-			failureList[val].EndFailureTime = splitted[0]
-			delete(failureIpAddrs, ipAddr)
+
+			// ある場合
+			pingResult := splitted[2]
+			if pingResult == "-" {
+				switch {
+				case val.TryCount+int64(1) == tryCount:
+					// 規定回数pingが通らない場合、故障リストにIPアドレスを追加する
+					failureList = append(failureList, &FailureServerDatum{
+						ipAddr,
+						val.StartTime,
+						"",
+					})
+					failureIpAddrs[ipAddr] = struct {
+						FailureListIndex int64
+						TryCount         int64
+						StartTime        string
+					}{
+						int64(len(failureList) - 1),
+						val.TryCount + 1,
+						val.StartTime,
+					}
+				case tryCount < val.TryCount:
+					// pingが規定回数以上通ってない場合は何もしない
+				case val.TryCount < tryCount:
+					failureIpAddrs[ipAddr] = struct {
+						FailureListIndex int64
+						TryCount         int64
+						StartTime        string
+					}{
+						0,
+						val.TryCount + 1,
+						val.StartTime,
+					}
+				}
+				continue
+			}
+
+			if splitted[2] != "-" {
+				switch {
+				case tryCount <= val.TryCount:
+					failureList[val.FailureListIndex].EndFailureTime = splitted[0]
+					delete(failureIpAddrs, ipAddr)
+				case val.TryCount < tryCount:
+					// 規定回数未満でpingが通った場合は故障mapから削除
+					delete(failureIpAddrs, ipAddr)
+				}
+
+			}
+			// 故障しているIPは復帰するまで処理不要
+			continue
 		}
 
-		// 故障（pingが通らない）の場合は故障マップにIPアドレスを追加する
 		if splitted[2] == "-" {
-			failureList = append(failureList, struct {
-				IpAddress        string
-				StartFailureTime string
-				EndFailureTime   string
+			// 故障（pingが通らない）の場合は故障マップにIPアドレスを追加する
+			failureIpAddrs[ipAddr] = struct {
+				FailureListIndex int64
+				TryCount         int64
+				StartTime        string
 			}{
-				ipAddr,
+				0,
+				1,
 				splitted[0],
-				"",
-			})
-			failureIpAddrs[ipAddr] = int64(len(failureList) - 1)
+			}
 		}
 	}
-	// handle first encountered error while reading
+
+	// ファイル読込中にエラーが起きた場合のハンドリング
 	if err := fileScanner.Err(); err != nil {
 		log.Fatalf("Error while reading file: %s", err)
 	}
 
+	ExportFailureList(failureList)
+}
+
+func ExportFailureList(failureList []*FailureServerDatum) {
 	for _, v := range failureList {
 		var endTime string
 		if v.EndFailureTime == "" {
 			endTime = "故障中"
 		} else {
-			// Pingが返ってきた寸前までが故障期間
+			// pingが返ってきた寸前までが故障期間
 			formatted, err := time.Parse("20060102150405", v.EndFailureTime)
+
 			if err != nil {
-				// TODO 日付フォーマット時のエラーのハンドリング
-				log.Println(err)
+				log.Printf("ログの日付形式が異常です。ログ: %s", v.EndFailureTime)
 			}
 
 			endTime = formatted.Add(-time.Second).Format("20060102150405")
